44fb1c40e04f8a9782b7c64c55e1cdeabb3532c8
sddtc <changhbaga@gmail.com>
2025-12-10 18:00:37 +0800

    [featuer] draw basic motto card

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
+import App from './app.jsx'
 
 const container = document.getElementById("root")
 const root = createRoot(container)
 
 root.render(
-  <BrowserRouter>
-    <div>hello, world.</div>
-  </BrowserRouter>
+  <div data-theme="light">
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </div>
 )
